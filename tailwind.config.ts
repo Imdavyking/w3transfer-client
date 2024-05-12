b715c31932983b6e22aeb0f27ec0852440f67cf3
Favour <gismozfexie@yahoo.com>
2024-05-12 13:49:14 +0100

    feat: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'gray-custom': {
+          'DEFAULT': '#F5F5F5',
+          100: '#D9D9D9',
+          200: '#8D8D8D',
+          300: '#1E1E1E',
+        },
+        accent: '#77FF60',
+        primary: '#1F2B69'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
