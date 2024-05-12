e83e18ea0eb580b8bfb73e3520d0299ad4750ed3
Favour <gismozfexie@yahoo.com>
2024-05-12 21:38:44 +0100

    chore: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = ({}: Props) => {
   return (
     <footer className='bg-white border-t border-gray-custom-100'>
       <div className="max-w-7xl w-full mx-auto px-4 md:px-8 bg-white py-5">
-        footer
+        <h2 className='text-xl font-bold text-gray-custom-100'>{ process.env.NEXT_PUBLIC_APP_NAME }</h2>
       </div>
     </footer>
   )
