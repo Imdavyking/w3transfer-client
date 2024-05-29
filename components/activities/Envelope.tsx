33029c582d71451b9902d60838ecba338b111aa3
Favour <gismozfexie@yahoo.com>
2024-05-29 02:07:12 +0100

    feat: partial update for activity component

--- components/activities/Envelope.tsx
+++ components/activities/Envelope.tsx
@@ -1,8 +1,19 @@
+import { subDays } from "date-fns"
+import Activity from "./Activity"
+
 type Props = {}
 
 const Envelope = (props: Props) => {
   return (
-    <div>Envelope</div>
+    <section className="grid gap-5">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <Activity
+          key={i}
+          title='Complete with DocuSign: mou-david-favour.pdf'
+          createdAt={subDays(new Date(), 3)}
+        />
+      ))}
+    </section>
   )
 }
 
