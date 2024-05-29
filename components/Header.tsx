6b63ae3625a5784c92e476831f80b1d6d5e1d980
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-05-29 18:00:33 +0100

    fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,7 @@ import {
   web3Enable,
   web3Accounts,
   web3FromAddress,
+  web3FromSource,
 } from "@polkadot/extension-dapp";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { useState } from "react";
@@ -17,6 +18,7 @@ import {
   InjectedExtension,
 } from "@polkadot/extension-inject/types";
 import Button from "@/components/Button";
+import { stringToHex } from "@polkadot/util";
 type Props = {};
 
 const ellipsis = (address: string): string => {
@@ -41,17 +43,46 @@ const Header = ({}: Props) => {
     InjectedAccountWithMeta[]
   >([]);
 
+  const signMessage = async () => {
+    if (accountConnected.length == 0) {
+      toast.error("Please connect your wallet first");
+      return;
+    }
+    const account = accountConnected[0];
+
+    // to be able to retrieve the signer interface from this account
+    // we can use web3FromSource which will return an InjectedExtension type
+    const injector = await web3FromSource(account.meta.source);
+
+    // this injector object has a signer and a signRaw method
+    // to be able to sign raw bytes
+    const signRaw = injector?.signer?.signRaw;
+
+    if (!!signRaw) {
+      // after making sure that signRaw is defined
+      // we can use it to sign our message
+      const { signature } = await signRaw({
+        address: account.address,
+        data: stringToHex(
+          "I am signing this message to prove that I own this account"
+        ),
+        type: "bytes",
+      });
+      console.log(signature);
+    }
+  };
   const connectExtension = async () => {
     let activeExtension: InjectedExtension[] = await web3Enable(
       config.APP_NAME
     );
 
     if (activeExtension.length == 0) {
-      console.log("hee");
       toast.error("Please install Polkadot extension to connect (Talisman)");
       return;
     }
     setActiveConnection(activeExtension);
+    // sign message
+    await signMessage();
 
     let accounts: InjectedAccountWithMeta[] = [];
     activeExtension
