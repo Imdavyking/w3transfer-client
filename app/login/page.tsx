0538eb89a8081aa7dce83c71ffec065dd1348e23
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-05-29 17:52:39 +0100

    email in login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,57 +1,58 @@
-'use client'
-import { useContext, useEffect, useLayoutEffect, useState } from 'react'
-import { HeaderContentContext } from '@/contexts';
-import Link from 'next/link';
-import Button from '@/components/Button';
-import { Input } from '@nextui-org/react';
-import { toast } from 'sonner';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useContext, useEffect, useLayoutEffect, useState } from "react";
+import { HeaderContentContext } from "@/contexts";
+import Link from "next/link";
+import Button from "@/components/Button";
+import { Input } from "@nextui-org/react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-type Props = {}
+type Props = {};
 
 const Login = (props: Props) => {
   const {
-    content, setContent,
-    contentRight, setContentRight,
-    contentLeft, setContentLeft
-  } = useContext(HeaderContentContext)
+    content,
+    setContent,
+    contentRight,
+    setContentRight,
+    contentLeft,
+    setContentLeft,
+  } = useContext(HeaderContentContext);
   useLayoutEffect(() => {
-    setContentRight!(() => (
-      <h2 className='md:text-xl'>Login</h2>
-    ))
-  }, [])
+    setContentRight!(() => <h2 className="md:text-xl">Login</h2>);
+  }, []);
 
   const [form] = useState({
-    email: 'user@gmail.com', // remove this in production
-  })
+    email: "user@gmail.com", // remove this in production
+  });
 
   const styles = {
     label: "text-gray-custom-200",
-    input: [
-      'placeholder:text-gray-custom-200/70',
-    ],
-    inputWrapper: [
-      'rounded',
-    ]
+    input: ["placeholder:text-gray-custom-200/70"],
+    inputWrapper: ["rounded"],
   };
 
-  const router = useRouter()
+  const router = useRouter();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if(!form.email) {
-      toast.error('Your wallet address has not been resolved. Please try again.')
-      return
+    e.preventDefault();
+    if (!form.email) {
+      toast.error(
+        "Your wallet address has not been resolved. Please try again."
+      );
+      return;
     }
     // navigate to the next page
-    router.push('/app')
-  }
+    router.push("/app");
+  };
 
   return (
-    <div className='flex-1 flex justify-center items-center'>
-      <div className='bg-white rounded p-6 md:p-20 w-[582px] md:max-w-[90%] border border-gray-custom-100'>
-        <h1 className='font-bold'>Login to {process.env.NEXT_PUBLIC_APP_NAME}</h1>
-        <form onSubmit={handleSubmit} className='flex flex-col gap-5 mt-5'>
-          <div className='space-y-5 select-none'>
+    <div className="flex-1 flex justify-center items-center">
+      <div className="bg-white rounded p-6 md:p-20 w-[582px] md:max-w-[90%] border border-gray-custom-100">
+        <h1 className="font-bold">
+          Login to {process.env.NEXT_PUBLIC_APP_NAME}
+        </h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5 mt-5">
+          <div className="space-y-5 select-none">
             <Input
               value={form.email}
               // onValueChange={(value) => setForm({ ...form, wallet: value })}
@@ -59,22 +60,20 @@ const Login = (props: Props) => {
               label="Your email address"
               variant="bordered"
               placeholder="user@gmail.com"
-              isReadOnly
-              isDisabled
               classNames={styles}
             />
           </div>
-          
-          <Button
-            type='submit'
-            className='uppercase'>
+
+          <Button type="submit" className="uppercase">
             Proceed
           </Button>
-          <Link className='flex self-end' href='/register'>Sign up for free</Link>
+          <Link className="flex self-end" href="/register">
+            Sign up for free
+          </Link>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
